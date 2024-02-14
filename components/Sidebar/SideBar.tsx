06f576ad2d2115f5a0b3de4d7af6f93c2d176ea6
NikeVir <82455622+NikeVir@users.noreply.github.com>
2024-02-14 18:22:21 +0530

    test 4.3

--- components/Sidebar/SideBar.tsx
+++ components/Sidebar/SideBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import logo from "@/app/assets/logo.svg";
 import currencyExchange from "@/app/assets/currency_exchange.svg";
@@ -10,7 +10,17 @@ import Profile from "./Profile";
 import metamask from "@/app/assets/metamask.svg";
 import { useSelectedLayoutSegment } from "next/navigation";
 import Link from "next/link";
-
+import { useChainId,useSwitchNetwork } from "wagmi";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { Cross2Icon } from "@radix-ui/react-icons";
 const navItemsList = [
   {
     image: currencyExchange,
@@ -34,7 +44,15 @@ const navItemsList = [
 
 const SideBar = () => {
   const segment = useSelectedLayoutSegment();
-
+  const chainId = useChainId();
+  const [open, setOpen] = React.useState(false);
+  const { chains, error, isLoading, pendingChainId, switchNetwork } =
+    useSwitchNetwork();
+  useEffect(() => {
+    if(chainId != 5){
+      setOpen(true)
+    }
+  }, []);
   return (
     <nav className="h-[100vh] min-w-[20vh] max px-[10px] py-2 sm:py-[10px] lg:py-[20px] xl:py-[30px]  bg-bgGrey flex flex-col items-center justify-between">
       <div className="flex flex-col items-center gap-[45px] fixed h-[100vh]">
@@ -72,6 +90,28 @@ const SideBar = () => {
             buttonText: "Logout",
           }}
         />
+
+       <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent style={{width:"300px", textAlign:"center",padding:"5vh"}} >
+            <DialogHeader className="flex items-start">
+              <DialogTitle className="text-textPrimary text-center font-medium  min-[1440px]:text-4xl 2dppx:text-2xl min-[1280px]:text-3xl text-2xl tracking-[-1.8px]">
+                Unsupported Network
+              </DialogTitle>
+            </DialogHeader>
+            <div>
+            <Button
+              variant={"primary"}
+              onClick={() => switchNetwork && switchNetwork(5)}
+              className="px-5 py-3 text-white"
+            >
+              Switch to Goerli
+            </Button>
+          </div>
+          </DialogContent>
+          
+        </Dialog>
+
+
       </div>
     </nav>
   );
