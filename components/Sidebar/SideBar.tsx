51d467a5b3082cda6554819fda4c12b6d1fe1b02
NikeVir <82455622+NikeVir@users.noreply.github.com>
2024-03-13 14:19:45 +0530

    darkmode

--- components/Sidebar/SideBar.tsx
+++ components/Sidebar/SideBar.tsx
@@ -107,7 +107,11 @@ const SideBar = () => {
           <label className="inline-flex items-center cursor-pointer">
   <input type="checkbox" value="" className="sr-only peer" onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")} />
   <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  
 </label>
+<div className="text-xs font-normal text-center text-textGrey dark:text-[#ffff]">
+    {resolvedTheme === "light" ? "Light" : "Dark"}
+  </div>
           </div>
         </div>
         <Profile
